f1a5ed1624a38832c9d8c7d4ba1638ba251d7c5e
RuiRamos84 <estgv18546@alunos.estgv.ipv.pt>
2022-10-27 03:29:58 +0100

    yt

--- blog/settings.py
+++ blog/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-n*84)3r@9o3*c%*mycn*6dfg(7vk^zx!_h&o-aj$o6dkt9odyc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -124,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
 
 
 
